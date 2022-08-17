749123d26246e3cf5f519a15bebc40f8b7e818a9
Nektow <99604672+Nektow@users.noreply.github.com>
2022-08-18 00:33:04 +0200

    Update matrice.py

--- matrice.py
+++ matrice.py
@@ -3,7 +3,8 @@ import random
 dictio = "azertyuiopqsdfghjkl mwxcvbn1234567890&œé(-è_çà)=*$ù!:;,><?./§%µ£+°@^`|[{#~"
 count = random.randint(0,1000)
 line = " "
-while True:
+while True: ### As here we have an infinite loop the program will run in a loop on your console.
+            ### Do not hesitate to press Ctrl + C to kill the program and thus stop the loop.
     for i in range(count):
         line += (random.choice(dictio))
     print(line)
