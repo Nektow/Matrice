09d3bdabfb5f25ac417aacc64e36fe9e51b960c2
Nektow <99604672+Nektow@users.noreply.github.com>
2023-12-07 14:42:17 +0100

    Update matrice.py

--- matrice.py
+++ matrice.py
@@ -1,11 +1,46 @@
 import random
+import time
+from rich.console import Console
 
-dictio = "azertyuiopqsdfghjkl mwxcvbn1234567890&œé(-è_çà)=*$ù!:;,><?./§%µ£+°@^`|[{#~"
-count = random.randint(0,1000)
-line = " "
-while True: ### As here we have an infinite loop the program will run in a loop on your console.
-            ### Do not hesitate to press Ctrl + C to kill the program and thus stop the loop.
+
+if __name__ == "__main__":
+    console = Console()
+    dictio = "azertyuiopqsdfghjkl mwxcvbn1234567890&œé(-è_çà)=*$ù!:;,><?./§%µ£+°@^`|[{#~"
+    line = " "
+    space ="  "
+    col = int(input("Choose a number between 0 and 255 for the color, if u choose 0 it's rainbow   "))
+    speed = int(input("Choose ur speed 0 : very slow, 1 : slow and 2 : fast as fuck boiiiiiii "))
+    rainbow = False
+
+
+### Ici est gérer l'option rainbow ###
+
+if col == 0 :
+    rainbow = True
+
+### Ici est gérer la vitesse ###
+
+if speed == 0 :
+    speed = 0.5
+elif speed == 1 :
+    speed = 0.03
+elif speed == 2:
+    speed = 0
+else : 
+    print("Wrong Values")
+
+
+
+while True:
+    time.sleep(speed)
+    count = random.randint(200,500)
     for i in range(count):
-        line += (random.choice(dictio))
-    print(line)
+        line += random.randint(0,2)*space + (random.choice(dictio)) #Génération de la ligne de char avec des espaces
+    if rainbow == True : ### Rainbow color choice ###
+        console.print(line, style = "color({})".format(random.randint(1,255)), highlight = False) 
+    else :
+        console.print(line, style = "color({})".format(col), highlight = False)
+    line = " "
+     
+
 
